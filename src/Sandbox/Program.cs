03e6e10f62c70b6100b4c6565b9f0ca0e2204916
Antony Male <antony.male@gmail.com>
2015-01-21 22:29:31 +0000

    First show at Sprache-based TOML parser

---
+++ src/Sandbox/Program.cs
@@ -0,0 +1,20 @@
+﻿using NToml;
+using Sprache;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = @"SomeKey = ""Some Value\t.\u00E9\"" \\ Yay!"" # This is a comment";
+            var result = Parser.KeyValuePair(new Input(str));
+        }
+    }
+}
